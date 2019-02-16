a9b66236ba0dbf4c9e2450fc8259f02a4ffbb3d7
Dmitry Brant <dbrant@wikimedia.org>
2019-02-16 12:10:07 -0500

    Remove extraneous stuff from DateUtil.

--- src/main/java/org/wikipedia/util/DateUtil.java
+++ src/main/java/org/wikipedia/util/DateUtil.java
@@ -1,8 +1,6 @@
 package org.wikipedia.util;
 
 import android.content.Context;
-import android.icu.text.RelativeDateTimeFormatter;
-import android.os.Build;
 import android.support.annotation.NonNull;
 
 import org.wikipedia.feed.model.UtcDate;
@@ -103,46 +101,6 @@ public final class DateUtil {
     @NonNull public static String yearToStringWithEra(int year) {
         Calendar cal = new GregorianCalendar(year, 1, 1);
         return getDateStringWithSkeletonPattern(cal.getTime(), year < 0 ? "y GG" : "y");
-    }
-
-    @NonNull public static String getYearDifferenceString(int year) {
-        int diffInYears = Calendar.getInstance().get(Calendar.YEAR) - year;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            switch (diffInYears) {
-                case 0:
-                    return RelativeDateTimeFormatter.getInstance().format(RelativeDateTimeFormatter.Direction.THIS, RelativeDateTimeFormatter.AbsoluteUnit.YEAR);
-                case 1:
-                    return RelativeDateTimeFormatter.getInstance().format(RelativeDateTimeFormatter.Direction.LAST, RelativeDateTimeFormatter.AbsoluteUnit.YEAR);
-                case -1:
-                    return RelativeDateTimeFormatter.getInstance().format(RelativeDateTimeFormatter.Direction.NEXT, RelativeDateTimeFormatter.AbsoluteUnit.YEAR);
-                default:
-                    return RelativeDateTimeFormatter.getInstance().format(diffInYears, RelativeDateTimeFormatter.Direction.LAST, RelativeDateTimeFormatter.RelativeUnit.YEARS);
-            }
-        } else {
-            Context context = WikipediaApp.getInstance().getApplicationContext();
-            return diffInYears == 0 ? context.getString(R.string.this_year)
-                    : context.getResources().getQuantityString(R.plurals.diff_years, diffInYears, diffInYears);
-        }
-    }
-
-    @NonNull public static String getDaysAgoString(int daysAgo) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            switch (daysAgo) {
-                case 0:
-                    return RelativeDateTimeFormatter.getInstance().format(RelativeDateTimeFormatter.Direction.THIS, RelativeDateTimeFormatter.AbsoluteUnit.DAY);
-                case 1:
-                    return RelativeDateTimeFormatter.getInstance().format(RelativeDateTimeFormatter.Direction.LAST, RelativeDateTimeFormatter.AbsoluteUnit.DAY);
-                case -1:
-                    return RelativeDateTimeFormatter.getInstance().format(RelativeDateTimeFormatter.Direction.NEXT, RelativeDateTimeFormatter.AbsoluteUnit.DAY);
-                default:
-                    return RelativeDateTimeFormatter.getInstance().format(daysAgo, RelativeDateTimeFormatter.Direction.LAST, RelativeDateTimeFormatter.RelativeUnit.DAYS);
-            }
-        } else {
-            Context context = WikipediaApp.getInstance().getApplicationContext();
-            // TODO: rename this string/plurals resource:
-            return daysAgo == 0 ? context.getResources().getString(R.string.view_continue_reading_card_subtitle_today)
-                    : context.getResources().getQuantityString(R.plurals.view_continue_reading_card_subtitle, daysAgo, daysAgo);
-        }
     }
 
     private DateUtil() {
