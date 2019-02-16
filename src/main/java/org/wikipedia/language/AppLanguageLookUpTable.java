add5da3fae7d0870a258a113b9e585cfeb586709
Dmitry Brant <dbrant@wikimedia.org>
2019-02-16 12:01:59 -0500

    Include language lookup table.

--- src/main/java/org/wikipedia/language/AppLanguageLookUpTable.java
+++ src/main/java/org/wikipedia/language/AppLanguageLookUpTable.java
@@ -7,7 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import org.wikipedia.R;
+import org.wikipedia.dataclient.R;
 
 import java.lang.ref.SoftReference;
 import java.util.Arrays;
