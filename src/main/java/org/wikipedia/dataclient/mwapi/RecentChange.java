2e4b89c4bb13c0313cb171ec177e68abcdaa3eaf
Dmitry Brant <dbrant@wikimedia.org>
2019-03-29 09:29:55 -0400

    Add more parameters to RecentChange.

--- src/main/java/org/wikipedia/dataclient/mwapi/RecentChange.java
+++ src/main/java/org/wikipedia/dataclient/mwapi/RecentChange.java
@@ -1,22 +1,42 @@
 package org.wikipedia.dataclient.mwapi;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 import com.google.gson.annotations.SerializedName;
 
+import org.apache.commons.lang3.StringUtils;
+
+@SuppressWarnings("unused")
 public class RecentChange {
-    private String type;
-    private String title;
-    @SerializedName("old_revid")
-    private String oldRevisionId;
+    @Nullable private String type;
+    @Nullable private String title;
+    private long pageid;
+    private long revid;
+    @SerializedName("old_revid") private long oldRevisionId;
+    @Nullable private String timestamp;
 
-    public String getType() {
-        return type;
+    @NonNull public String getType() {
+        return StringUtils.defaultString(type);
     }
 
-    public String getTitle() {
-        return title;
+    @NonNull public String getTitle() {
+        return StringUtils.defaultString(title);
     }
 
-    public String getOldRevisionId() {
+    public long getPageId() {
+        return pageid;
+    }
+
+    public long getRevId() {
+        return revid;
+    }
+
+    public long getOldRevisionId() {
         return oldRevisionId;
+    }
+
+    public String getTimestamp() {
+        return StringUtils.defaultString(timestamp);
     }
 }
