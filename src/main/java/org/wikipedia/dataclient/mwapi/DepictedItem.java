1b56e7fb8892513398a2d5b2726276e6aa05d8ae
vanshikaarora <vanshikaa937@gmail.com>
2019-07-11 18:03:31 +0530

    squash commits

---
+++ src/main/java/org/wikipedia/dataclient/mwapi/DepictedItem.java
@@ -0,0 +1,22 @@
+package org.wikipedia.dataclient.mwapi;
+
+public class DepictedItem {
+
+    private String searchinfo;
+
+    private String search;
+
+    private String success;
+
+    public String getSearchinfo() {
+        return searchinfo;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+}
